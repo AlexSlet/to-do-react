0cb966fc30eb67ee4b4c3ba9f51ff16b0c5ef34c
AlexSlyota <oleksii.slota@inventorsoft.co>
2023-09-25 15:54:42 +0300

    basic to-do

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,61 @@ import { useState } from "react";
 import Button from "@mui/material/Button";
 import ItemsModal from "./components/ItemsModal";
 import "./App.scss";
+import { ToDoItem } from "./interfaces";
+
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState<ToDoItem[]>([]);
+  const [todoToEdit, setTodoToEdit] = useState<ToDoItem | null>(null)
 
-  function saveTodo(title: string, description: string) {
-    console.log(title, description);
-    
+  function saveTodo(todo: ToDoItem) {
+    setTodoList((prevState) => [
+      ...prevState,
+      { title: todo.title, description: todo.description, id: new Date().getTime() },
+    ]);
+    setTodoToEdit(null)
   }
+
+  function editTodo(todo: ToDoItem) {
+    const index = todoList.findIndex(item => item.id === todo.id)
+    const listCopy = [...todoList]
+    listCopy[index] = todo
+    setTodoList(listCopy)
+    setTodoToEdit(null)
+  }
+
+  function onRemove(id: number) {
+    const filteredList = todoList.filter(item => item.id !== id)
+    setTodoList(filteredList)
+  }
+
+  function onEdit(todo: ToDoItem) {
+    setTodoToEdit(todo)
+  }
+
+  const renderList = todoList.map((todo) => {
+    return (
+      <li className="toDo-list--item" key={todo.id}>
+        <div>
+          <h3>{todo.title}</h3>
+          <p>{todo.description}</p>
+        </div>
+        <div>
+          <Button onClick={() => onEdit(todo)}>Edit</Button>
+          <Button color="error" onClick={() => onRemove(todo.id)}>Remove</Button>
+        </div>
+      </li>
+    );
+  });
 
   return (
     <>
       <div className="toDo">
         <div className="main-title">
           <h1>To-do list</h1>
-          <ItemsModal onSave={saveTodo}/>
+          <ItemsModal todoToEdit={todoToEdit} onSave={saveTodo} onEdit={editTodo} onCancel={() => setTodoToEdit(null)}/>
         </div>
-        <ul className="toDo-list">
-          <li className="toDo-list--item">
-            <div>
-              <h3>Do this</h3>
-              <p>description</p>
-            </div>
-            <div>
-              <Button>Edit</Button>
-              <Button color="error">Remove</Button>
-            </div>
-          </li>
-        </ul>
+        <ul className="toDo-list">{renderList}</ul>
       </div>
     </>
   );
--- src/components/ItemsModal.tsx
+++ src/components/ItemsModal.tsx
@@ -6,7 +6,8 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Stack } from "@mui/material";
 import { Add } from "@mui/icons-material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { ToDoItem } from "../interfaces";
 
 const style = {
   position: "absolute" as "absolute",
@@ -24,16 +25,61 @@ const style = {
   gap: "20px",
 };
 
-export default function ItemsModal({onSave}: {onSave: Function}) {
-  const [open, setOpen] = useState(false);
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+interface ItemsModal {
+  onSave: Function;
+  onEdit: Function;
+  onCancel: Function;
+  todoToEdit: ToDoItem | null;
+}
 
-  function handleSave () {
-    onSave(title, description)
+export default function ItemsModal({
+  onSave,
+  onEdit,
+  onCancel,
+  todoToEdit = null,
+}: ItemsModal) {
+  const [open, setOpen] = useState(false);
+  const [todo, setTodo] = useState<ToDoItem>({
+    title: "",
+    description: "",
+    id: 0,
+  });
+
+  useEffect(() => {
+    if (todoToEdit) {
+      setTodo(todoToEdit);
+      setOpen(true);
+    }
+  }, [todoToEdit]);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false)
+    onCancel()
+  };
+  const setTodoProp = (props: Object) => {
+    setTodo({
+      ...todo,
+      ...props,
+    });
+  };
+
+  function handleSave() {
+    if (todo.id) {
+      onEdit(todo);
+    } else {
+      onSave(todo);
+    }
+    setTodoProp({
+      title: "",
+      description: "",
+      id: 0
+    });
+    setOpen(false);
   }
+  const isDisabled = (): boolean => {
+    return !(todo.title && todo.description);
+  };
 
   return (
     <div>
@@ -50,16 +96,20 @@ export default function ItemsModal({onSave}: {onSave: Function}) {
           <h2>Create To-Do</h2>
           <TextField
             label="Title"
-            value={title}
+            value={todo.title}
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setTitle(event.target.value);
+              setTodoProp({
+                title: event.target.value,
+              });
             }}
           />
           <TextField
             label="Description"
-            value={description}
+            value={todo.description}
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setDescription(event.target.value);
+              setTodoProp({
+                description: event.target.value,
+              });
             }}
           />
           <Stack
@@ -67,12 +117,12 @@ export default function ItemsModal({onSave}: {onSave: Function}) {
             alignItems="center"
             direction="row"
           >
-            <Button
-              onClick={handleSave}
-            >
-              Save
+            <Button onClick={handleSave} disabled={isDisabled()}>
+              {todoToEdit ? "Edit" : "Save"}
             </Button>
-            <Button color="error" onClick={handleClose}>Cancel</Button>
+            <Button color="error" onClick={handleClose}>
+              Cancel
+            </Button>
           </Stack>
         </Box>
       </Modal>
